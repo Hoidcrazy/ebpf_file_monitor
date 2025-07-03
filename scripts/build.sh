441f41860aaa9de843ab41e4e0088ad5f9af8979
jy-centos-chane <270987682@qq.com>
2025-07-03 17:18:04 +0800

    修改ebpf cmakelists

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PROJECT_ROOT=$(cd "$SCRIPT_DIR/.." && pwd)
 
 # 创建并进入 build 目录（在项目根目录下）
+rm -rf "$PROJECT_ROOT/build"
 mkdir -p "$PROJECT_ROOT/build"
 cd "$PROJECT_ROOT/build"
 
