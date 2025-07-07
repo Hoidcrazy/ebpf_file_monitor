69df78057a5a6ce23fecce285c219aaaa26881f5
jy-centos-chane <270987682@qq.com>
2025-07-07 16:49:16 +0800

    修复cmake

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 确保在项目根目录执行
 cd "$(dirname "$0")/.."
 
 # 初始化子模块
@@ -12,7 +13,8 @@ cd build
 # 运行CMake配置
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_TESTS=OFF \
+    -DBUILD_TESTS=ON \
+    -DENABLE_ASAN=OFF \
     -DCMAKE_INSTALL_PREFIX=../install
 
 # 编译项目
@@ -26,7 +28,10 @@ mkdir -p ../bin
 cp ../install/bin/* ../bin/
 cp src/ebpf/*.bpf.o ../bin/
 cp src/ebpf/*.skel.h ../bin/
-cp external/libbpf/build/tools/bpftool/bpftool ../bin/
+
+# 复制测试文件
+mkdir -p ../tests/test_docs
+cp ../tests/test_docs/test_content.txt ../tests/test_docs/
 
 echo "构建完成！可执行文件在 bin/ 目录"
 echo "运行: sudo ./scripts/run.sh"
